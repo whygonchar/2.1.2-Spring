d55a7b1f5d6cf2b8eb605388bf4e94497ed06e77
Daniil <danyamsk09@gmail.com>
2024-12-25 13:32:23 +0300

    Задача выполнена

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean2.getMessage());
+
+        System.out.println(bean == bean2);
+        System.out.println(catBean == catBean2);
     }
 }
